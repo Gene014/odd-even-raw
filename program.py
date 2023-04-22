1e95f2b2833430808233d6af6b880a6bec2c1e35
Gene014 <calmaeugene06@gmail.com>
2023-04-22 22:56:00 +0800

    creating output error

--- program.py
+++ program.py
@@ -3,3 +3,8 @@
 #The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
 
 #create the ff txt file, must be same path of the current program.
+
+def process():
+    # open numbers.txt (read), double txt even.txt (append), triple odd.txt (append)
+    with open("numbers.txt" , "r") as input_file, open("even.txt" , "a") as even_file, open ("odd.txt" , "a") as odd_file:
+        #read numbers.txt line by line
